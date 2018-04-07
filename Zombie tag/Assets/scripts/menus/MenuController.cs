c00cc5812ecc242110ff3efdf60da0fb5aeea9ab
DESKTOP-Henry <hbennett@uccs.edu>
2018-04-06 19:31:52 -0600

    Added in the basic menu system. This includes a basic temp controlls menu, and the ability to enter the game from the main menu. I will continue to work on the GPS feature of the menu later on this week.

---
+++ Zombie tag/Assets/scripts/menus/MenuController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+/// <summary>
+/// A basic class used to handle menu inputs
+/// </summary>
+public class MenuController : MonoBehaviour
+{
+    // Called 50 times per second
+    public void FixedUpdate()
+    {
+        // Load the main menu if user presses 'esc'
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            LoadMainMenu();
+        }
+    }
+    // Called when changing scene to the game
+    public void LoadDemoLevel()
+    {
+        SceneManager.LoadScene("City_scene_winter");
+    }
+
+    // Called when changing scene to the control menu
+    public void LoadControls()
+    {
+        SceneManager.LoadScene("HelpMenu");
+    }
+
+    // Called when changing scene to the main menu
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
